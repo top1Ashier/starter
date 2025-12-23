7cf0960a2a477d819500519ef4b194256a229190
top1Ashier <touchskyc915@gmail.com>
2025-12-23 16:01:35 +0800

    self: update clangd configuration

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,3 +4,11 @@ local servers = { "clangd", "lua_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+vim.lsp.config.clangd = {
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header_insertion=nerver",
+  }
+}
