4862f810e3970abe68ba0da0e40c4fec3f88d8db
top1ashier <top1ashier@users.noreply.github.com>
2026-07-05 18:42:46 +0800

    migrate lsp config to vim.lsp.config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,28 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "clangd", "pyright"  }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+local servers = { "clangd", "pyright" }
+
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
 end
 
--- lspconfig.clangd.setup {
---     cmd = {
---         "clangd",
---         "--background-index",
---         "--clang-tidy",
---         "--header-insertion=never",
---     },
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
--- }
+vim.lsp.enable(servers)
