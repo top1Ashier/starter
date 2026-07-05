e9e4c6173019191d0c64ec7d37cade51faca11aa
top1ashier <top1ashier@users.noreply.github.com>
2026-07-05 18:45:40 +0800

    add trouble diagnostics plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -86,6 +86,44 @@ return {
   },
 
   {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {},
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics",
+      },
+      {
+        "<leader>xs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols",
+      },
+      {
+        "<leader>xl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List",
+      },
+    },
+  },
+
+  {
     "echasnovski/mini.cursorword",
     version = false,
     lazy = false,
